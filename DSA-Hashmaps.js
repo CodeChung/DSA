58da56b98f19bc5e3510c483569a42bd29ad3159
Harry Chung <hc9825@gmail.com>
2019-08-11 22:03:06 -0700

    hashmaps

---
+++ DSA-Hashmaps.js
@@ -0,0 +1,100 @@
+class HashMap {
+    constructor(initialCapacity=8) {
+        this.length = 0;
+        this._hashTable = [];
+        this._capacity = initialCapacity;
+        this._deleted = 0;
+    }
+
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i = 0; i < string.length; i++) {
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            hash = hash & hash;
+        }
+        return hash >>> 0;
+    }
+    get(key) {
+        const index = this._findSlot(key);
+        if (this._hashTable[index] === undefined) {
+            throw new Error('Key error');
+        }
+        return this._hashTable[index].value;
+    }
+    set(key, value){
+        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        }
+        //Find the slot where this key should be in
+        const index = this._findSlot(key);
+
+        if(!this._hashTable[index]){
+            this.length++;
+        }
+        this._hashTable[index] = {
+            key,
+            value,
+            DELETED: false
+        }; 
+    }
+    delete(key) {
+        const index = this._findSlot(key);
+        const slot = this._hashTable[index];
+        if (slot === undefined) {
+            throw new Error('Key error');
+        }
+        slot.DELETED = true;
+        this.length--;
+        this._deleted++;
+    }
+
+    _findSlot(key) {
+        const hash = HashMap._hashString(key);
+        const start = hash % this._capacity;
+
+        for (let i=start; i<start + this._capacity; i++) {
+            const index = i % this._capacity;
+            const slot = this._hashTable[index];
+            if (slot === undefined || (slot.key === key && !slot.DELETED)) {
+                return index;
+            }
+        }
+    }
+
+    _resize(size) {
+        const oldSlots = this._slots;
+        this._capacity = size;
+        // Reset the length - it will get rebuilt as you add the items back
+        this.length = 0;
+        this._slots = [];
+
+        for (const slot of oldSlots) {
+            if (slot !== undefined) {
+                this.add(slot.key, slot.value);
+            }
+        }
+    }
+}
+
+function main() {
+    const lor = new HashMap()
+    lor.MAX_LOAD_RATIO = .5
+    lor.SIZE_RATIO = 3
+    
+    lor.set('Hobbit', 'Bilbo')
+    lor.set('Hobbit', 'Frodo')
+    lor.set('Wizard', 'Gandolf')
+    lor.set('Human', 'Aragorn')
+    lor.set('Elf', 'Legolas')
+    lor.set('Maiar', 'The Necromancer')
+    lor.set('Maiar', 'Sauron')
+    lor.set('RingBearer', 'Gollum')
+    lor.set('LadyOfLight', 'Galadriel')
+    lor.set('HalfElven', 'Arwen')
+    lor.set('Ent', 'Treebeard')
+    console.log('Maiar key:', lor.get('Maiar'))
+    console.log('Hobbit key:', lor.get('Hobbit'))
+  
+    return lor
+}
